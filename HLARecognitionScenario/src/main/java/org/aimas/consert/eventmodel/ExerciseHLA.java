b7385e75bb46e6f6a04ace6115ab79697e94c407
David Iancu <davidiancu@yahoo.com>
2017-05-17 13:45:49 +0300

    resolved typos

--- HLARecognitionScenario/src/main/java/org/aimas/consert/eventmodel/ExerciseHLA.java
+++ HLARecognitionScenario/src/main/java/org/aimas/consert/eventmodel/ExerciseHLA.java
@@ -2,7 +2,7 @@ package org.aimas.consert.eventmodel;
 
 
 /*
- *   Class for modeling a dining  high level activity
+ *   Class for modeling an exercise  high level activity
 */
 public class ExerciseHLA extends HLA {
     
