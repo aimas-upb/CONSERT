a9c9909a42b84f3d80244c359285ddef6c7373f7
Alexandru Sorici <alex.sorici@gmail.com>
2017-05-22 15:53:28 +0300

    #22 Moved accumulateTest.drl to separate folder in resources.

--- HLARecognitionScenario/src/test/java/org/aimas/consert/unittest/AccumulateWithTemporalExtensionTest.java
+++
@@ -1,9 +0,0 @@
-package org.aimas.consert.unittest;
-
-public class AccumulateWithTemporalExtensionTest {
-	
-	public AccumulateWithTemporalExtensionTest() {
-		// TODO Auto-generated constructor stub
-	}
-	
-}
