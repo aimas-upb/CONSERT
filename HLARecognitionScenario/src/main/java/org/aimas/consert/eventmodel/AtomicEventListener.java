601b7b8533fa795f11e73cd1535fef855d27306e
Alexandru Sorici <alex.sorici@gmail.com>
2017-06-16 12:05:35 +0300

    #8 Changing packages: splitting into model, engine and different types of test (e.g. hla, wsu).

--- HLARecognitionScenario/src/main/java/org/aimas/consert/eventmodel/AtomicEventListener.java
+++
@@ -1,7 +0,0 @@
-package org.aimas.consert.eventmodel;
-
-import java.util.EventListener;
-
-public interface AtomicEventListener extends EventListener {
-	void eventInserted(AtomicEventInserted event);
-}
