e4a04938a9ee26c7713c2c804f562c6211d414eb
David Iancu <davidiancu@yahoo.com>
2017-06-20 11:57:32 +0300

    made junit test in separate method

--- consert-engine/src/test/java/org/aimas/consert/unittest/AccumulateTestRunner.java
+++ consert-engine/src/test/java/org/aimas/consert/unittest/AccumulateTestRunner.java
@@ -16,64 +16,62 @@ import org.junit.Test;
 public class AccumulateTestRunner extends TestSetup {
 
 	@Test
-    public static void main(String[] args) {
-    	
-    	try {
-	    	// load up the knowledge base
+	public void test1() {
+		try {
+
+			// load up the knowledge base
 //	        KieServices ks = KieServices.Factory.get();
 //	        KieSessionConfiguration config = ks.newKieSessionConfiguration();
 //	        config.setOption(ClockTypeOption.get("realtime"));
-//	        
+//
 //		    KieContainer kContainer = ks.getKieClasspathContainer();
 //	    	KieSession kSession = kContainer.newKieSession("ksession-rules", config);
 
-    		KieSession kSession = getKieSessionFromResources( "accumulate_test_rules/accumulateTest.drl" );
-            final List<Long> results = new ArrayList<Long>();
+			KieSession kSession = getKieSessionFromResources( "accumulate_test_rules/accumulateTest.drl" );
+			final List<Long> results = new ArrayList<Long>();
 
-            kSession.setGlobal( "results", results );
-    		
-	    	// set up engine runner thread and event inserter
-	    	Thread engineRunner = new Thread(new EngineRunner(kSession));
-	    	
-	    	EventTracker eventTracker = new EventTracker(kSession);
-	    	TestInserter eventInserter = new TestInserter(eventTracker);
-	    	
-	    	// start the engine thread and the inserter, wait for the inserter to finish then exit
-	    	engineRunner.start();
-	    	eventInserter.start();
-	    	
-	    	while (!eventInserter.isFinished()) {
-	    		Thread.sleep(2000);
-	    	}
-	    	
-	    	eventInserter.stop();
-	    	
-	    	// verify if results only contain values <= 1 
-	    	System.out.println(results);
+			kSession.setGlobal( "results", results );
+
+			// set up engine runner thread and event inserter
+			Thread engineRunner = new Thread(new EngineRunner(kSession));
+
+			EventTracker eventTracker = new EventTracker(kSession);
+			TestInserter eventInserter = new TestInserter(eventTracker);
+
+			// start the engine thread and the inserter, wait for the inserter to finish then exit
+			engineRunner.start();
+			eventInserter.start();
+
+			while (!eventInserter.isFinished()) {
+				Thread.sleep(2000);
+			}
+
+			eventInserter.stop();
+
+			// verify if results only contain values <= 1
+			System.out.println(results);
 			for (Long l : results) {
 				Assert.assertTrue("IT DOES NOT WORK! GOT A COUNT GREATER THAN 1.",l<=1);
 			}
 			System.out.println("WE HAVE A WINNER. ALL COUNTS WITHIN WINDOW ARE <=1.");
-	    	
-	    	kSession.halt();
-	    	kSession.dispose();
-	    	
-	    	engineRunner.join(1000);
-    	}
-    	catch(Exception ex) {
-    		ex.printStackTrace();
-    	}
-    	
-    	
-    	/*
-    	HLATest test = new HLATest();
 
-        File inputFile = getFileNameFromResources("files/single_hla_120s_01er_015fd.json");
-        Queue<Object> events = JSONEventReader.parseEvents(inputFile);
+			kSession.halt();
+			kSession.dispose();
 
-        System.out.println(events);
-        */
+			engineRunner.join(1000);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public static void main(String[] args) {
+		AccumulateTestRunner A = new AccumulateTestRunner();
+		A.test1();
+		
     }
-    
-    
+
+
+
 }
