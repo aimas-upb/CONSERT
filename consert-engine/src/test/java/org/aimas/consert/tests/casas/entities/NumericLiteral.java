0d95f30dae10daaddab2670b9fff549c349884b9
Alexandru Sorici <alex.sorici@gmail.com>
2017-08-13 15:29:57 +0300

    #35 Adding hashCode and equals methods for String and Numeric literal ContextEntities.

--- consert-engine/src/test/java/org/aimas/consert/tests/casas/entities/NumericLiteral.java
+++ consert-engine/src/test/java/org/aimas/consert/tests/casas/entities/NumericLiteral.java
@@ -1,6 +1,7 @@
 package org.aimas.consert.tests.casas.entities;
 
 import org.aimas.consert.model.content.ContextEntity;
+import org.eclipse.rdf4j.model.impl.SimpleValueFactory;
 
 public class NumericLiteral implements ContextEntity {
 	
@@ -22,9 +23,32 @@ public class NumericLiteral implements ContextEntity {
 
 	@Override
     public String getEntityId() {
-	    // TODO Auto-generated method stub
-	    return null;
+		return SimpleValueFactory.getInstance().createLiteral(value).getLabel();
     }
-	
-	
+
+	@Override
+    public int hashCode() {
+	    final int prime = 31;
+	    int result = 1;
+	    result = prime * result + ((value == null) ? 0 : value.hashCode());
+	    return result;
+    }
+
+	@Override
+    public boolean equals(Object obj) {
+	    if (this == obj)
+		    return true;
+	    if (obj == null)
+		    return false;
+	    if (getClass() != obj.getClass())
+		    return false;
+	    NumericLiteral other = (NumericLiteral) obj;
+	    if (value == null) {
+		    if (other.value != null)
+			    return false;
+	    }
+	    else if (!value.equals(other.value))
+		    return false;
+	    return true;
+    }
 }
