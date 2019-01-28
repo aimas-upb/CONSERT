f36490937cad07acdfbc9550b77936f811bb3a3e
Alexandru Sorici <alex.sorici@gmail.com>
2019-01-28 10:39:05 +0200

    #62 Removing old packaging of core engine

--- consert-engine/src/main/java/org/aimas/consert/engine/AtomicEventInserted.java
+++
@@ -1,7 +0,0 @@
-package org.aimas.consert.engine;
-
-import org.aimas.consert.model.content.ContextAssertion;
-
-public interface AtomicEventInserted {
-	ContextAssertion getInsertedObject();
-}
--- consert-engine/src/main/java/org/aimas/consert/engine/AtomicEventListener.java
+++
@@ -1,7 +0,0 @@
-package org.aimas.consert.engine;
-
-import java.util.EventListener;
-
-public interface AtomicEventListener extends EventListener {
-	void eventInserted(AtomicEventInserted event);
-}
